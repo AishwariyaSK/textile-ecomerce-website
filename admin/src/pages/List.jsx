eb09362bd7659da5f26c4be14195bd9e0541d7b5
AishwariyaSK <124779433+AishwariyaSK@users.noreply.github.com>
2025-03-14 13:28:46 +0530

    update admin

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import axios from 'axios';
 import { backendurl, currency } from '../App';
-import { set } from 'mongoose';
 import { toast } from 'react-toastify';
 
 const List = () => {
